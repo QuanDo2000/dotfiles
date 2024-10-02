364d679541b4846eb11a048b417bfe0b7b7e17fb
Quan Do <minhquand3@gmail.com>
2024-10-01 20:28:44 -0700

    Re-add PsReadline

--- windows/Powershell/Microsoft.VSCode_profile.ps1
+++ windows/Powershell/Microsoft.VSCode_profile.ps1
@@ -2,3 +2,14 @@ Invoke-Expression (&starship init powershell)
 
 # PSGet
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+# PSReadLine Options
+Import-Module Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
