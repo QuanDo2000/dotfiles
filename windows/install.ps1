a7cff280ab3db16687b184300bfd5b42d86c2509
Quan Do <minhquand3@gmail.com>
2024-11-03 00:31:36 -0700

    Remove Cygwin and Neovim

--- windows/install.ps1
+++ windows/install.ps1
@@ -18,17 +18,8 @@ function CopyDirWithBackup($source, $destination) {
     }
 }
 
-function InstallLazyVim {
-    Move-Item $env:LOCALAPPDATA\nvim $env:LOCALAPPDATA\nvim.bak
-    Move-Item $env:LOCALAPPDATA\nvim-data $env:LOCALAPPDATA\nvim-data.bak
-
-    git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
-
-    Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
-}
-
 function InstallPackages {
-    winget install Microsoft.Powershell Cygwin.Cygwin vim.vim Git.Git Microsoft.VisualStudioCode Microsoft.WindowsTerminal JanDeDobbeleer.OhMyPosh Neovim.Neovim --disable-interactivity --accept-package-agreements
+    winget install Microsoft.Powershell vim.vim Git.Git Microsoft.VisualStudioCode Microsoft.WindowsTerminal JanDeDobbeleer.OhMyPosh --disable-interactivity --accept-package-agreements
 
     Install-Module -Name PowerShellGet -Force
     Install-Module PSReadLine -AllowPrerelease -Force
@@ -40,8 +31,6 @@ function InstallPackages {
         Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
         Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
     }
-
-    InstallLazyVim
 }
 
 function InstallFont {
