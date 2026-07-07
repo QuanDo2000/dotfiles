a50899133ede3b4114ea2347ffbff4bd89c2b064
Quan Do <minhquand3@gmail.com>
2026-07-07 13:29:00 -0700

    Remove stale lazy.nvim version comments

--- tests/bash/test_nixos_config.sh
+++ tests/bash/test_nixos_config.sh
@@ -383,6 +383,7 @@ test_home_config_owns_existing_xdg_configs() {
   assert_not_contains "$lazy_text" "lazyrepo"
   assert_not_contains "$lazy_text" "git\", \"clone"
   assert_not_contains "$lazy_text" "enabled = true, -- check for plugin updates"
+  assert_not_contains "$lazy_text" "latest git commit"
   assert_contains "$lazy_text" "run dotfile update"
   assert_contains "$home_text" "xdg.configFile.\"fcitx5\""
   assert_contains "$home_text" "./unix/config/fcitx5"
